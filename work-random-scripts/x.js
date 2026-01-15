04b6ebbf17f268d5f5606c301cb0dd9283327bc7
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2026-01-15 11:19:23 -0600

    Refactor background style assignments in x.js

--- work-random-scripts/x.js
+++ work-random-scripts/x.js
@@ -1,8 +1,8 @@
-let a = document.querySelector.bind(document);
-let x = a("div#header").style;
+let x = document.querySelector("div#header").style;
 let b = document.body.style;
-x.background = "url('https://static.wikia.nocookie.net/c182800e-d3b0-40d2-8cd1-b2c077816705/scale-to-width/755') no-repeat center";
+
+x.background = "url(\"https://static.wikia.nocookie.net/c182800e-d3b0-40d2-8cd1-b2c077816705/scale-to-width/755\") no-repeat center";
 x.backgroundSize = "cover";
-a(".mainDiv").style.backgroundColor = "transparent";
-b.background = "url('https://wallpapers.com/images/featured/funny-cat-v24i6v24vmtk4ygu.jpg') no-repeat center";
-b. backgroundSize = "cover";
+document.querySelector(".mainDiv").style.backgroundColor = "none";
+b.background = "url(\"https://wallpapers.com/images/featured/funny-cat-v24i6v24vmtk4ygu.jpg\") no-repeat center";
+b.backgroundSize = "cover";
