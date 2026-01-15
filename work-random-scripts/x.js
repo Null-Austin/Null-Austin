1a4f2e6cb224428e208a68f167f28acd9ea60406
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2026-01-15 09:34:40 -0600

    Refactor querySelector usage and update styles

--- work-random-scripts/x.js
+++ work-random-scripts/x.js
@@ -1,8 +1,9 @@
-let a = document.querySelector;
+let a = (selector) => document.querySelector(selector);
 let x = a("div#header").style;
-let b = document.body.style
-x.background="url(\"https://static.wikia.nocookie.net/c182800e-d3b0-40d2-8cd1-b2c077816705/scale-to-width/755\") no-repeat center";
-x.backgroundSize="cover";
-a(".mainDiv").style.backgroundColor="none";
-b.background="url(\"https://cataas.com/cat\") no-repeat center";
-b.backgroundSize="cover";
+let b = document.body.style;
+
+x.background = "url(\"https://static.wikia.nocookie.net/c182800e-d3b0-40d2-8cd1-b2c077816705/scale-to-width/755\") no-repeat center";
+x.backgroundSize = "cover";
+a(".mainDiv").style.backgroundColor = "transparent";
+b.background = "url(\"https://cataas.com/cat\") no-repeat center";
+b.backgroundSize = "cover";
