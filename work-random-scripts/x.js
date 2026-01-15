62fde395460bea72fe03b33c0985bcd1098761c8
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2026-01-15 09:32:21 -0600

    Add background images to header and body styles

---
+++ work-random-scripts/x.js
@@ -0,0 +1,8 @@
+let a = document.querySelector;
+let x = a("div#header").style;
+let b = document.body.style
+x.background="url(\"https://static.wikia.nocookie.net/c182800e-d3b0-40d2-8cd1-b2c077816705/scale-to-width/755\") no-repeat center";
+x.backgroundSize="cover";
+a(".mainDiv").style.backgroundColor="none";
+b.background="url(\"https://cataas.com/cat\") no-repeat center";
+b.backgroundSize="cover";
