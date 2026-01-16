14ec59657c5832c4073435a366e9324951bc30b6
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2026-01-16 09:50:28 -0600

    reverted change

--- work-random-scripts/x.js
+++ work-random-scripts/x.js
@@ -1,14 +1,8 @@
-document.querySelectorAll("*",e=>{
-    e.style.fontFamily = "Papyrus";
-    e.style.background = "unset";
-    e.style.backgroundColor = "";
-});
+let x = document.querySelector("div#header").style;
+let b = document.body.style;
 
-document.querySelector('div#header').style.background = 'url(\'https://static.wikia.nocookie.net/c182800e-d3b0-40d2-8cd1-b2c077816705/scale-to-width/755\') no-repeat center';
-document.querySelector('div#header').style.backgroundSize = 'cover';
-document.body.style.background = 'url(\'https://wallpapers.com/images/featured/funny-cat-v24i6v24vmtk4ygu.jpg\') no-repeat center';
-document.body.style.backgroundSize = 'cover';
-document.querySelector('.mainDiv').style.backgroundColor = '';
-
-import styles from 'https://cdn.jsdelivr.net/gh/null-austin/pb-assets/Papyrus-Regular-stylesheet.css' assert { type: 'css' };
-document.adoptedStyleSheets = [styles];
+x.background = "url(\"https://static.wikia.nocookie.net/c182800e-d3b0-40d2-8cd1-b2c077816705/scale-to-width/755\") no-repeat center";
+x.backgroundSize = "cover";
+document.querySelector(".mainDiv").style.backgroundColor = "none";
+b.background = "url(\"https://wallpapers.com/images/featured/funny-cat-v24i6v24vmtk4ygu.jpg\") no-repeat center";
+b.backgroundSize = "cover";
