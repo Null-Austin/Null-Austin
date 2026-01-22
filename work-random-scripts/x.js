c3f11fadce429f28fce9a242c52f313200934632
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2026-01-22 11:23:13 -0600

    Change header background image to a monkey image

--- work-random-scripts/x.js
+++ work-random-scripts/x.js
@@ -1,7 +1,7 @@
 let x = document.querySelector("div#header").style;
 let b = document.body.style;
 
-x.background = "url(\"https://static.wikia.nocookie.net/c182800e-d3b0-40d2-8cd1-b2c077816705/scale-to-width/755\") no-repeat center";
+x.background = "url(\"https://purepng.com/public/uploads/large/purepng.com-monkeymonkeyfunnyanimalwildchimpanzeeape-981524651620xnkin.png\") no-repeat center";
 x.backgroundSize = "cover";
 document.querySelector(".mainDiv").style.backgroundColor = "none";
 b.background = "url(\"https://wallpapers.com/images/featured/funny-cat-v24i6v24vmtk4ygu.jpg\") no-repeat center";
